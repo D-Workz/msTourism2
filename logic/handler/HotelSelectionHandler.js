d4a7e98ed6c511fd5f55d7419b84e69835cb051d
alex-bl <alexander.blaas@student.uibk.ac.at>
2018-01-14 01:31:20 +0100

    app-crash-fix if no hotel is found on selection

--- logic/handler/HotelSelectionHandler.js
+++ logic/handler/HotelSelectionHandler.js
@@ -12,20 +12,23 @@ class HotelSelectionHandler {
         }
 
         db.mfindOne({type: /Hotel/, "annotation.name": new RegExp(hotelName, "i")}).then((data) => {
-            if (Array.isArray(data)) {
-                data.forEach((entry) => {
-                    app.db().save("selectedHotel", data, (err) => {
-                        console.log("Attribute 'selectedHotel' set with content of '" + data.annotation.name + "'");
-                        app.ask("What do you want to know? I can give you a description, information about rooms and prices, the location, contact infos and the average rating.");
-                    });
-                });
-            } else {
-                app.db().save("selectedHotel", data, (err) => {
-                    console.log("Attribute 'selectedHotel' set with content of '" + data.annotation.name + "'");
-                    app.ask("What do you want to know? I can give you a description, information about rooms and prices, the location, contact infos and the average rating.");
-                });
-            }
-
+        	if(data.length>0){
+	            if (Array.isArray(data)) {
+	                data.forEach((entry) => {
+	                    app.db().save("selectedHotel", data, (err) => {
+	                        console.log("Attribute 'selectedHotel' set with content of '" + data.annotation.name + "'");
+	                        app.ask("What do you want to know? I can give you a description, information about rooms and prices, the location, contact infos and the average rating.");
+	                    });
+	                });
+	            } else {
+	                app.db().save("selectedHotel", data, (err) => {
+	                    console.log("Attribute 'selectedHotel' set with content of '" + data.annotation.name + "'");
+	                    app.ask("What do you want to know? I can give you a description, information about rooms and prices, the location, contact infos and the average rating.");
+	                });
+	            }
+        	}else{
+        		app.ask("I'm sorry. I couldn't find any Hotel with name '"+hotelName+"'")
+        	}            
         });
 
 
