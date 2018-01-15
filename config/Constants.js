83f76af7cbb4aba84aaaa3bb6eaf6ac0915fe041
alex-bl <alexander.blaas@student.uibk.ac.at>
2018-01-15 20:02:54 +0100

    constants updated

--- config/Constants.js
+++ config/Constants.js
@@ -1,6 +1,13 @@
-const AVAILABLE_THINGS_STRING = "Hotel, store, restaurant, tourist attraction, ski resort, pub, bar, bank, museum and train station";
+const AVAILABLE_THINGS_TEXT = "Hotel, store, restaurant, tourist attraction, ski resort, pub, bar, bank, museum and train station";
+const AVAILABLE_CHANGER_TEXT = "You can change the city as well as the thing you want to talk about."
+const AVAILABLE_PROPERTIES_TEXT = "You can ask for description, contact, address, stars, things nearby and distance to the city center on all things."+
+"Additionally you can ask for rooms, price and beds for Hotels.";
+const AVAILABLE_FILTER_TEXT = "You can filter Hotels either by saying 'better rated than' or 'cheaper than'."
 
 
 module.exports = {
-		AVAILABLE_THINGS : AVAILABLE_THINGS_STRING
+		AVAILABLE_THINGS : AVAILABLE_THINGS_TEXT,
+		AVAILABLE_PROPERTIES : AVAILABLE_PROPERTIES_TEXT,
+		AVAILABLE_CHANGER : AVAILABLE_CHANGER_TEXT,
+		AVAILABLE_FILTER : AVAILABLE_FILTER_TEXT		
 }
